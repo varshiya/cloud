d386da8c3dba333a3999d7c1d011e4520cbb13a2
197400587 <197400587@qq.com>
2020-12-14 16:23:58 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>hello cloud</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      envId: "",
+      callFunctionResult: "",
+    };
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    async callFunction() {
+      try {
+        const res = await this.$cloudbase.callFunction({
+          name: "vue-echo",
+          data: {
+            foo: "bar",
+          },
+        });
+        this.callFunctionResult = res;
+      } catch (e) {
+        console.error(e)
+        this.callFunctionResult = e.message;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+
+  .hello {
+    max-width: 500px;
+    margin: 0 auto;
+    word-break: break-all;
+  }
+</style>
